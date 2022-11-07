dddef069f4d5ff4db5448e74b93d12499410bd29
zctmdc <zctmdc@outlook.com>
2022-11-08 04:43:29 +0800

    fixbug: build Big ,version not set

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -18,7 +18,7 @@ LOG_WARNING "Try Build: version_b_s_rc - ${version_b_s_rc}"
 if [[ "${version_b_s_rc}" == "v2s" || "${version_b_s_rc}" == "v2" || "${version_b_s_rc}" == "v1" ]]; then
     LOG_WARNING "BUILD BIG VERSION"
     . init_path.sh
-    cp ${PROJECT_ROOT_DIR}/Linux/n2n_${BIG_VERSION}/* $RESULT_DIR
+    cp ${PROJECT_ROOT_DIR}/Linux/n2n_${version_b_s_rc}/* $RESULT_DIR
     exit 0
 fi
 
