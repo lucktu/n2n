8a5b00171f82aae105ab6176be99f2d24e4c4970
zctmdc <zctmdc@outlook.com>
2022-11-12 04:01:32 +0800

    修复顺序，loop

--- scripts/fixlib.sh
+++ scripts/fixlib.sh
@@ -5,6 +5,13 @@ command_exists() {
     command -v "$@" >/dev/null 2>&1
 }
 
+if [[ -n "$(/usr/local/sbin/edge -h 2>&1 | grep /lib/ld-linux.so.3)" ]]; then
+    LOG_WARNING 缺少 /lib/ld-linux.so.3 , 正在修复
+    find / -name ld-linux*.so* | head -n 1 | xargs -I {} ln -sv {} /lib/ld-linux.so.3
+    LOG_WARNING 缺少 /lib/ld-linux.so.3 , 修复完毕
+    sh -c /tmp/n2n-lucktu/scripts/fixlib.sh
+fi
+
 if [[ -n "$(/usr/local/sbin/edge -h 2>&1 | grep libcrypto.so.1.0.0)" ]]; then
     LOG_WARNING 缺少 libssl1.0.0 , 正在修复
     if command_exists apt-get; then
@@ -15,10 +22,6 @@ if [[ -n "$(/usr/local/sbin/edge -h 2>&1 | grep libcrypto.so.1.0.0)" ]]; then
         apk add --no-cache --upgrade libssl1.0
     fi
     LOG_WARNING 缺少 libssl1.0.0 , 修复完毕
+    sh -c /tmp/n2n-lucktu/scripts/fixlib.sh
 fi
 
-if [[ -n "$(/usr/local/sbin/edge -h 2>&1 | grep /lib/ld-linux.so.3)" ]]; then
-    LOG_WARNING 缺少 /lib/ld-linux.so.3 , 正在修复
-    find / -name ld-linux*.so* | head -n 1 | xargs -I {} ln -sv {} /lib/ld-linux.so.3
-    LOG_WARNING 缺少 /lib/ld-linux.so.3 , 修复完毕
-fi
